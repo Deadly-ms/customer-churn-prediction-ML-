43baed5de4babc5a9a486fd8ba2b356b4104ce90
Deadly-ms <pearlzone143@gmail.com>
2026-08-11 13:27:46 +0530

    update app.py for deploying

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,6 @@ app.register_blueprint(api)
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5050,
-        debug=True
+        port=int(os.environ.get("PORT", 5050)),
+        debug=os.environ.get("FLASK_DEBUG", "false").lower() == "true"
     )
